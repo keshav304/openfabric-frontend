d4725175cd7b00f7b7177565d0f832f448bec717
keshav jha <keshavjhaa2678@gmail.com>
2023-05-15 14:58:19 +0530

    Update environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://localhost:8000/api' // Replace with your actual API URL
+    apiBaseUrl: 'https://openfabric-backend-c1e5.onrender.com/api' // Replace with your actual API URL
   };
